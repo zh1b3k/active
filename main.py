c599b9a53e735d278f2891e597975db00fae7fe4
zh1b3k <zhibekseidakhmet@yahoo.com>
2024-09-08 12:15:10 +0400

    commit ke 1

---
+++ main.py
@@ -0,0 +1,42 @@
+import os
+from datetime import date, time, datetime
+import datetime
+import random
+
+
+if __name__=="__main__":
+    total_day = 365 #total days back
+    commit_frequency = 10 #commit time per day
+    repo_link = "https://github.com/zh1b3k/active.git" #"https://github.com/aliifam/github-activity-generator.git"
+
+    tl = total_day #time day
+    ctr = 1
+
+    now = datetime.datetime.now()
+
+    f = open("commit.txt", "w")
+    os.system("git config user.name")
+    os.system("git config user.email")
+    os.system("git init")
+
+    pointer = 0
+
+    while tl > 0:
+        ct = random.randint(0, commit_frequency)
+        while ct > 0:
+            f = open("commit.txt", "a+")
+            l_date = now + datetime.timedelta(days=-pointer)
+            formatdate = l_date.strftime("%Y-%m-%d")
+            f.write(f"commit ke {ctr}: {formatdate}\n")
+            f.close()
+            os.system("git add .")
+            os.system(f"git commit --date=\"{formatdate} 12:15:10\" -m \"commit ke {ctr}\"")
+            print(f"commit ke {ctr}: {formatdate}")
+            ct-=1
+            ctr+=1
+        pointer+=1
+        tl-=1
+
+    os.system(f"git remote add origin {repo_link}")
+    os.system("git branch -M main")
+    os.system("git push -u origin main -f")
